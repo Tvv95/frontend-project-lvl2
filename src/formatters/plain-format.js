27814eb2f0b20e4124fdbf9635e7d284d9afac67
Tvv95 <truvladislav@gmail.com>
2020-03-10 15:46:00 +0300

    fix eslint

--- src/formatters/plain-format.js
+++ src/formatters/plain-format.js
@@ -9,31 +9,32 @@ const plainFormatFunc = (ast) => {
   const reducer = (acc, currentChild) => {
     switch (currentChild.type) {
       case 'unchanged':
-        acc += `Property '${currentChild.key}' was unchanged\n`;
+        acc.push(`Property '${currentChild.key}' was unchanged\n`);
         break;
       case 'added':
-        acc += `Property '${currentChild.key}' was added with value: ${quotesCheck(currentChild.value)}\n`;
+        acc.push(`Property '${currentChild.key}' was added with value: ${quotesCheck(currentChild.value)}\n`);
         break;
       case 'deleted':
-        acc += `Property '${currentChild.key}' was deleted\n`;
+        acc.push(`Property '${currentChild.key}' was deleted\n`);
         break;
       case 'changed':
-        acc += `Property '${currentChild.key}' was changed from ${quotesCheck(currentChild.changedValues[0])} to ${quotesCheck(currentChild.changedValues[1])}\n`;
+        acc.push(`Property '${currentChild.key}' was changed from ${quotesCheck(currentChild.changedValues[0])} to ${quotesCheck(currentChild.changedValues[1])}\n`);
         break;
       case 'hasChild':
         currentChild.children.map((current) => {
-          current.key = `${currentChild.key}.${current.key}`;
-          return current;
+          const newCurrent = Object.assign(current);
+          newCurrent.key = `${currentChild.key}.${current.key}`;
+          return newCurrent;
         });
-        acc += currentChild.children.reduce(reducer, '');
+        acc.push(currentChild.children.reduce(reducer, []));
         break;
       default:
         break;
     }
     return acc;
   };
-  const plainPreResult = ast.reduce(reducer, '');
-  return plainPreResult.replace(/object Object/gi, 'complex value').slice(0, -1);
+  const plainPreResult = ast.reduce(reducer, []);
+  return plainPreResult.join('').replace(/,/gi, '').replace(/object Object/gi, 'complex value').slice(0, -1);
 };
 
 export default plainFormatFunc;
